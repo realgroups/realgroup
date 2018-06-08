de2e60c56f0a1544d31fb65b9e73bd9ba5a435d8
Lee, Won-hee <merit@outlook.kr>
2018-06-09 00:39:42 +0900

    git commit

--- models/member.js
+++ models/member.js
@@ -5,7 +5,7 @@ var MemberSchema = new mongoose.Schema({
     name: {type: String, required: true, max: 25},
     phone: {type: String, required: true, unique: true},
     email: {type: String},
-    role: {type: Number, required}
+    role: {type: Number, required: true}
 });
 
 module.exports = mongoose.model('Member', MemberSchema);
