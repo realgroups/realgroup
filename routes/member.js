09240a1aa0349f54061f0c87da5769b588cbc022
Lee, Won-hee <merit@outlook.kr>
2018-06-09 00:22:24 +0900

    git commit

--- routes/member.js
+++ routes/member.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 // Require the controllers WHICH WE DID NOT CREATE YET!!
-var member_controller = require('../controllers/member');
+var member_controller = require('./controllers/member');
 
 router.get('/test', member_controller.test);
 
