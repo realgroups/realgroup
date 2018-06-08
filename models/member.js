eb5c187966c1730c4c5b0c2d1cc87e6c3648545d
Lee, Won-hee <merit@outlook.kr>
2018-06-09 00:38:08 +0900

    git commit

--- models/member.js
+++ models/member.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema;
 
-var MemberSchema = new Schema({
+var MemberSchema = new mongoose.Schema({
     name: {type: String, required: true, max: 25},
     phone: {type: String, required: true, unique: true},
     email: {type: String},
