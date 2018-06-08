e19a3b6bc46dca35dc992ae0f58de8bd878a917a
Lee, Won-hee <merit@outlook.kr>
2018-06-09 00:26:17 +0900

    git commit

--- Controllers/member.js
+++ Controllers/member.js
@@ -1,6 +1,5 @@
 var Member = require('../models/mamber');
 
-//Simple version, without validation or sanitation
 exports.test = function (req, res) {
     res.send('Greetings from the Test controller!');
 };
