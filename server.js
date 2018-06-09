9faf3dec38f2f8131a1d5741c9579c20dca2e435
Lee, Won-hee <merit@outlook.kr>
2018-06-09 14:21:38 +0900

    git commit

--- server.js
+++ server.js
@@ -6,6 +6,10 @@ var app     = express();
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
+app.get('/', function (req, res) {
+  res.render('index.html', { pageCountMessage : null});
+});
+
 app.use('/members', member);
 
 app.listen(port, ip);
