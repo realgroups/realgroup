cb31bee64ce164c7a8d1927168613e9b47c8d6ef
Lee, Won-hee <merit@outlook.kr>
2018-06-09 14:25:07 +0900

    git commit

--- server.js
+++ server.js
@@ -6,11 +6,21 @@ var app     = express();
 var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
 
+
+app.engine('html', require('ejs').renderFile);
+app.use(morgan('combined'));
+
 app.get('/', function (req, res) {
   res.render('index.html', { pageCountMessage : null});
 });
 
 app.use('/members', member);
+
+// error handling
+app.use(function(err, req, res, next){
+  console.error(err.stack);
+  res.status(500).send('Something bad happened!');
+});
 
 app.listen(port, ip);
 console.log('Server running on http://%s:%s', ip, port);
@@ -23,14 +33,9 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 
 
 
-app.engine('html', require('ejs').renderFile);
-app.use(morgan('combined'));
 
-// error handling
-app.use(function(err, req, res, next){
-  console.error(err.stack);
-  res.status(500).send('Something bad happened!');
-});
+
+
 
 app.listen(port, ip, () => {
   console.log('Server running on http://%s:%s', ip, port);
